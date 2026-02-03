6892f26d8ed7211aed20926346e2a3429de51cb5
Mike Janzen M Agcopra <mjagcopra21441@liceo.edu.ph>
2026-02-03 12:00:04 +0800

    Add AppComponent and PostsComponent, update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PostsComponent }
+];
+
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('adet-fe-bsit22');
+}
